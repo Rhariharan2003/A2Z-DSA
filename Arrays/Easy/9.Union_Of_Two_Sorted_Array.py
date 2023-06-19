c155df00c708e5c74b123c6fabd57001181d8ad8
Rhariharan2003 <136108485+Rhariharan2003@users.noreply.github.com>
2023-06-19 17:17:19 +0530

    Create 9.Union_Of_Two_Sorted_Array.py

---
+++ Arrays/Easy/9.Union_Of_Two_Sorted_Array.py
@@ -0,0 +1,40 @@
+#User function Template for python3
+#Problem Link -> https://practice.geeksforgeeks.org/problems/union-of-two-sorted-arrays-1587115621/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=union-of-two-sorted-arrays
+class Solution:
+    
+    #Function to return a list containing the union of the two arrays.
+    def findUnion(self,a,b,n,m):
+        '''
+        :param a: given sorted array a
+        :param n: size of sorted array a
+        :param b: given sorted array b
+        :param m: size of sorted array b
+        :return:  The union of both arrays as a list
+        '''
+        # code here 
+        k = set(a+b)
+        s = list(k)
+        s.sort()
+        return s
+       
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# Contributed by : Nagendra Jha
+# Modified by : Sagar Gupta
+
+
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases) :
+        n,m = map(int,input().strip().split())
+        a = list(map(int,input().strip().split()))
+        b = list(map(int,input().strip().split()))
+        ob=Solution()
+        li = ob.findUnion(a,b,n,m)
+        for val in li:
+            print(val, end = ' ')
+        print()
+# } Driver Code Ends
