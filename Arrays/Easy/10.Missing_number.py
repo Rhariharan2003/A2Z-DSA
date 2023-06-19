15b5279d85e832e02bd211b4513aa2036cd35628
Rhariharan2003 <136108485+Rhariharan2003@users.noreply.github.com>
2023-06-19 19:20:53 +0530

    Create 10.Missing_number.py

---
+++ Arrays/Easy/10.Missing_number.py
@@ -0,0 +1,35 @@
+#User function Template for python3
+#Problem Link -> https://practice.geeksforgeeks.org/problems/missing-number4257/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=missing-number
+def missingNumber(A, N):
+     # Your code goes here
+    ans = N*(N+1)//2
+    tot = 0
+    for i in A:
+       tot += i 
+    return (ans-tot)
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+def main():
+
+    T = int(input())
+
+    while(T > 0):
+        n = int(input())
+        a = [int(x) for x in input().strip().split()]
+        print(missingNumber(a, n))
+
+        T -= 1
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+    
+# } Driver Code Ends
