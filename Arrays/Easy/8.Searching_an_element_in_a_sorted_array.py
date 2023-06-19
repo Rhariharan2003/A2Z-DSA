f952b99520b8a7c26e521b2d478508731c1954c8
Rhariharan2003 <136108485+Rhariharan2003@users.noreply.github.com>
2023-06-19 16:49:55 +0530

    Create 8.Searching_an_element_in_a_sorted_array.py

---
+++ Arrays/Easy/8.Searching_an_element_in_a_sorted_array.py
@@ -0,0 +1,28 @@
+#User function Template for python3
+#Problem Link -> https://practice.geeksforgeeks.org/problems/who-will-win-1587115621/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=who-will-win
+class Solution:
+    ##Complete this function
+    def searchInSorted(self,arr, N, K):
+        #Your code here
+        for i in range(len(arr)):
+            if arr[i] == K:
+                return 1
+        return -1
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import math
+if __name__ == '__main__': 
+	t = int(input())
+	for _ in range(t):
+		NK = input().strip().split()
+		N = int(NK[0])
+		K = int(NK[1])
+		A = [ int(x) for x in input().strip().split() ]
+		ob=Solution()
+		print(ob.searchInSorted(A,N,K))
+
+# } Driver Code Ends
